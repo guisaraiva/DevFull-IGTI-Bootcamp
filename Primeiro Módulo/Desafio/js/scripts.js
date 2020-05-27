f0d15b76b86bde4a53fe62bb5a9201440ebdafd2
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-05-27 11:40:03 -0300

    Organizando estrutura e atualizando os arquivos

--- Primeiro Módulo/Desafio/js/scripts.js
+++ Primeiro Módulo/Desafio/js/scripts.js
@@ -1,40 +1,65 @@
-window.addEventListener('load', start)
+// Declaração das variáveis que serão utilizadas no projeto.
+let countlMen = 0;
+let countWomen = 0;
+let sumAges = 0;
+let averageAges =0;
 
+let countCountries = 0;
 
+let tabCountries = null;
 
-function start(){
+let totalContactsList = 0;
 
-  const btnPesquisaUsuario = document.querySelector("#btnPesquisaUsuario")
-  btnPesquisaUsuario.addEventListener("click", event => {
-  event.PreventDefault();
+let allContacts = [];
+
+window.addEventListener('load', ()=>{
+  countCountries = document.querySelector('#countCountries');
+  tabCountries = document.querySelector('#tabCountries');
+  getContacts();
 })
-const url = "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo";
-fetch(url).then(response =>{
-  return response.json();
-}).then(data=>{
-  if(data.erro)
-  {
-  alert("O CEP DIGITADO ESTÁ INVÁLIDO");
-  return ;
-  }
-  atribuirCampos(data);
-})
+
+async function getContacts (){
+  const contactList = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
+  const dataContacts = await contactList.json();
+
+  allContacts = dataContacts.results.map(contact => {
+    const { name, dob, picture, gender } = contact;    // Realizando um destructuring
+      return {
+        name: name,
+        ageContact: dob.age,
+        photo: picture,
+        gender: gender
+      }
+  });
+  //console.log(allContacts);
+  render();
 }
 
-function atribuirCampos(data){
-  const picture = document.querySelector("#picture");
-  picture.value = data.picture;
+function render(){
+  // Executar todas as funções posteriores.
+  renderContactsList();
+  renderSummaryContacts();
+}
+function renderContactsList(){
+  let contactsHTML = "<div>";
+  console.log(allContacts);
+  allContacts.forEach(contact => {
+    const { name, ageContact, photo, gender } = contact;
+    const contactHTML = `
+    <div class="country">
+       <img src="${photo.thumbnail}" alt="Image" />
+       ${name.first} 
+       ${name.last} 
+       ,${ageContact} anos
+      </div>
+    </div>    
+    `
+    contactsHTML += contactHTML;
+  })
+  contactsHTML += "</div>";
+  tabCountries.innerHTML = contactsHTML;
 }
 
-
-
-
-
-
-
-
-
-
-function buscaDadosApi(){
-  fetch("https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo")
+function renderSummaryContacts(){
+  countCountries.textContent = allContacts.length;
 }
