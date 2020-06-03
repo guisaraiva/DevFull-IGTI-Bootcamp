2f19360a698ff09583fbab3f4efd41925447c781
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-06-02 22:45:43 -0300

    Implementação do json.length

--- 2º - Módulo - API/Trabalho Prático/index.js
+++ 2º - Módulo - API/Trabalho Prático/index.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 let files = []
 
 
-  fs.readFile('./data/Estados.json','utf8', function (err, data){
+  fs.readFile('./data/Cidades.json','utf8', function (err, data){
      let guardaNome = [];
         // console.log(JSON.parse(data))
         let json = JSON.parse(data);
     //json = Object.keys(json.Name);
-      for (let i =0; i< json.length; i++){
-          guardaNome[i] = json[i].Nome;
-      }
-      console.log(guardaNome)
-   fs.reFile('./data/example.json',json, function(err){
-      console.log('Arquivo criado.')
-    })
+      // for (let i =0; i< json.length; i++){
+      //     guardaNome[i] = json[i].Nome;
+      // }
+      console.log(json.length)
+  //  fs.reFile('./data/example.json',json, function(err){
+  //     console.log('Arquivo criado.')
+  //   })
     // // console.log(json.Nome);
    // console.log(nome)
         //  files = {Estado: Nome}
@@ -23,8 +23,8 @@ let files = []
 
 // fs.readdirSync('./data').forEach(files => {
 //   fs.readFile('./data/'+files,'utf8', function (err, data){
-  
-//     console.log(JSON.parse(data.name))
+//     //Implementar um laço de repetição para guardar as informações.
+//     //Olhar os tamanhos dos arquivos..
 //   })
 // })
   // globalCidades =[];
